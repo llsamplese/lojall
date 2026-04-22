857e53cba9cbcfd01f90128093ad58bc9eee677b
llsamples <llsamplesegravacoes@gmail.com>
2026-04-22 13:55:08 -0300

    Split coupon visibility for home and direct pages

--- data/store-config-defaults.js
+++ data/store-config-defaults.js
@@ -1,7 +1,9 @@
 module.exports = {
   ui: {
-    showCouponBox: true,
-    showAvailableCouponsPanel: true,
+    showCouponBoxHome: true,
+    showCouponBoxDirect: true,
+    showAvailableCouponsPanelHome: true,
+    showAvailableCouponsPanelDirect: true,
     showPackagesPanel: true,
     showPackagesPanelHome: true,
     showPackagesPanelDirect: true,
