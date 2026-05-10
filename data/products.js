71e0c1ee19c6a2e2d0f720c7848507d3d0b6a679
llsamples <llsamplesegravacoes@gmail.com>
2026-05-10 15:25:34 -0300

    Move product catalog to shared data file

---
+++ data/products.js
@@ -0,0 +1,625 @@
+const LL_PRODUCTS = [
+  {
+    "nome": "LL Drums Menos é Mais",
+    "valor": 75,
+    "video": "https://youtu.be/mJjQR7U2kAw"
+  },
+  {
+    "nome": "LL Drums Filho do Piseiro",
+    "valor": 70,
+    "video": "https://youtu.be/fQa6_yS6Zi8?si=WOVFCiuFOR0dRhh_"
+  },
+  {
+    "nome": "LL Acordeon Forró v2",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=9R_xmNK0M4c"
+  },
+  {
+    "nome": "LL Bass Forró",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=Am54KWkecBM"
+  },
+  {
+    "nome": "LL Arrocha Synths",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=zzbEJUs9MkE"
+  },
+  {
+    "nome": "LL Piseiro Drums Cyber",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=SWTd-foPoyA"
+  },
+  {
+    "nome": "LL Drums Yasmin Sensação",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=qKLD2xA2m64"
+  },
+  {
+    "nome": "LL Drums Laercia Dantas",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=fhNn5la2l5Q"
+  },
+  {
+    "nome": "LL Drums Eduardinho dos Teclados",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=I6yIpJ5tHeM"
+  },
+  {
+    "nome": "LL Drums Toque Dez",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=sxXJIoUFYQ4"
+  },
+  {
+    "nome": "LL Drums Diovane Severo",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=c7_Bj2z5tlA"
+  },
+  {
+    "nome": "LL Guitarra Forró",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=76wm2WOucCs"
+  },
+  {
+    "nome": "LL Pacote Produção Guilherme Silva",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=GF9Vlq8qOys"
+  },
+  {
+    "nome": "LL Pacote Produção Arrocha",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=YIuHRLtGSjU"
+  },
+  {
+    "nome": "LL Drums Luxúria",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=n8eJ6Q1Nq9A"
+  },
+  {
+    "nome": "LL Drums Forró Boys",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=zjt75FJMJmM"
+  },
+  {
+    "nome": "LL Drums Rey Vaqueiro",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=geBo5YDSsH4"
+  },
+  {
+    "nome": "LL Piano Romântico",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=JgX_3tA4pGw&t=5s"
+  },
+  {
+    "nome": "LL Acordeon Forro",
+    "valor": 70,
+    "video": "https://drive.google.com/file/d/1t5-yc4FcREU4z-m175sGIprgYBBwWnJ-/view?usp=sharing"
+  },
+  {
+    "nome": "LL Pacote Produção Claudio Ney e Juliana",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=J46vaIavi38"
+  },
+  {
+    "nome": "LL Drums Claudio Ney e Juliana",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=J46vaIavi38"
+  },
+  {
+    "nome": "LL Drums Pablo",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=lb9FINdRGf4&t=8s"
+  },
+  {
+    "nome": "LL Drums Safadao Vaquejada",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=D-JNlJU4VDQ"
+  },
+  {
+    "nome": "LL Violao Pop v3",
+    "valor": 90,
+    "video": "https://www.youtube.com/watch?v=k5tclbUGABc"
+  },
+  {
+    "nome": "LL Drums Thales Play",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=14pxiab-sYQ&pp=0gcJCQMKAYcqIYzv"
+  },
+  {
+    "nome": "LL Drums Flavio Pizada Quente",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=8iLazzVOBiA"
+  },
+  {
+    "nome": "LL Drums Forrozin Os Feras",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=3ElrjIbD60U"
+  },
+  {
+    "nome": "LL Pacote Producao Arrasta-Pe (Completo)",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=B-VnCfYLIj8"
+  },
+  {
+    "nome": "LL Pacote Producao Arrasta-Pe (Acomp)",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=B-VnCfYLIj8"
+  },
+  {
+    "nome": "LL Drums Arrasta-Pé v2",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=B-VnCfYLIj8"
+  },
+  {
+    "nome": "LL Drums Guilherme Silva",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=egMYYporhgs"
+  },
+  {
+    "nome": "LL Pacote Produção Nattan",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=-u2iGSJMYc8"
+  },
+  {
+    "nome": "LL Keys Pagodao",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=8TE-0kQYAgE"
+  },
+  {
+    "nome": "LL Bass Pagodao v2",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=DrEmHweA0aU"
+  },
+  {
+    "nome": "LL Guitarra Pagodao v3",
+    "valor": 80,
+    "video": "https://www.youtube.com/watch?v=JmGsTXqFDU8"
+  },
+  {
+    "nome": "LL Drums Marcynho Batidao",
+    "valor": 70,
+    "video": "https://youtu.be/8V3I303NcWU"
+  },
+  {
+    "nome": "LL Bass Forro Romantico",
+    "valor": 70,
+    "video": "https://youtu.be/L8wgsiVASb0?si=K_wrF7f1Y-wC6x5A"
+  },
+  {
+    "nome": "LL Drums Leo Foguete",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=oIeRu7Le_iI"
+  },
+  {
+    "nome": "LL Drums Seresta do Rasta",
+    "valor": 70,
+    "video": "https://youtu.be/Jl7dWRJVg5k?si=L8gWlKQ5LQzFmAz7"
+  },
+  {
+    "nome": "LL Drums Desejo",
+    "valor": 70,
+    "video": "https://youtu.be/edbpb1HAJpM?si=J007U-ehQcdUPUYB"
+  },
+  {
+    "nome": "LL Drums Nattan Forró",
+    "valor": 70,
+    "video": "https://youtu.be/O0bMyQ9-reo?si=MFPWuXzaiekS-Ff6"
+  },
+  {
+    "nome": "LL Baião Drums",
+    "valor": 60,
+    "video": "https://youtu.be/q3uNB_Fts_M?si=mSP9s7Uobw-49iDY"
+  },
+  {
+    "nome": "LL Drums Arrocha Safadão",
+    "valor": 70,
+    "video": "https://youtu.be/pA9_SCV2bPs?si=WIXq-aKjMMPu2-KL"
+  },
+  {
+    "nome": "LL Bass Arrocha v2 – Seresta",
+    "valor": 70,
+    "video": "https://youtu.be/YnpQGQoL0i4?si=801jOc0ykr9aNH5c"
+  },
+  {
+    "nome": "LL Guitarra Arrocha v2 – Seresta",
+    "valor": 70,
+    "video": "https://youtu.be/uZHO0a083Oo?si=jHw5I-r7somFev9x"
+  },
+  {
+    "nome": "LL Drums Natanzinho Lima",
+    "valor": 70,
+    "video": "https://youtu.be/Knm8FoJ-z8M?si=E0wif0lIRbAt7IGN"
+  },
+  {
+    "nome": "LL Drums Henry Freitas",
+    "valor": 60,
+    "video": "https://youtu.be/xTR6MuZM1lU?si=aZmLhWs7_Q3xeB9v"
+  },
+  {
+    "nome": "LL Lambada Drums",
+    "valor": 60,
+    "video": "https://youtu.be/hSMQr-Jb978?si=RFGTX_6zH325DTPA"
+  },
+  {
+    "nome": "LL Calypso Drums",
+    "valor": 60,
+    "video": "https://youtu.be/DyMQWGG-eww?si=akWcilP1UY6ZKvfx"
+  },
+  {
+    "nome": "LL Ukulele Prime",
+    "valor": 70,
+    "video": "https://youtu.be/Nz4ld0Bgd2k?si=SOfwzRx1ah7byiSl"
+  },
+  {
+    "nome": "LL Arraia Drums",
+    "valor": 60,
+    "video": "https://youtu.be/ij0IMy274_A?si=BFDgociu8m9dFmon"
+  },
+  {
+    "nome": "LL Arrocha Drums v2",
+    "valor": 70,
+    "video": "https://youtu.be/ev5jrDTqB2Q?si=E0ZmxbFXY-0Eh2wT"
+  },
+  {
+    "nome": "LL Forro Romantico Drums v2",
+    "valor": 70,
+    "video": "https://youtu.be/yl484BqOu9w?si=SDv5syQUQKEBnYcW"
+  },
+  {
+    "nome": "LL Forrozao Drums v1.1",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=jEp5ZXmy0ik"
+  },
+  {
+    "nome": "LL Forro Vaneirao Drums",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=J2SWJlwefvo"
+  },
+  {
+    "nome": "LL Violao Pop 6-8",
+    "valor": 80,
+    "video": "https://youtu.be/IZwjw__8pUw?si=MlB63xE8R900ERMw"
+  },
+  {
+    "nome": "LL Strings",
+    "valor": 60,
+    "video": "https://youtu.be/V5zqi4J0sWc?si=tJZFNLJ8IAknCE23"
+  },
+  {
+    "nome": "LL Acordeon Arrasta-Pe",
+    "valor": 70,
+    "video": "https://youtu.be/0gKTeQ0OJj0?si=kw9slO03p-DBL5K8"
+  },
+  {
+    "nome": "LL Violao Pop v2",
+    "valor": 80,
+    "video": "https://youtu.be/jWlLr_bRMCM?si=agB7sYHkC7IdF7ml"
+  },
+  {
+    "nome": "LL Bass Pagode",
+    "valor": 60,
+    "video": "https://youtu.be/Egn7F4w9ziQ?si=7PYeXL3GtEIbx5TR"
+  },
+  {
+    "nome": "LL Bass Forrozao",
+    "valor": 60,
+    "video": "https://youtu.be/IEPn3MSkjJI?si=saLm4IQzeljGZNlb"
+  },
+  {
+    "nome": "LL Acordeon Forrozao",
+    "valor": 60,
+    "video": "https://www.youtube.com/watch?v=SOjvoOpe6jA&list=RDSOjvoOpe6jA&start_radio=1"
+  },
+  {
+    "nome": "LL Guitarra Forrozao",
+    "valor": 60,
+    "video": "https://youtu.be/czN2vM06eqM?si=1FzGrgYj5dMwz_U5"
+  },
+  {
+    "nome": "LL Bass Pagodao",
+    "valor": 70,
+    "video": "https://youtu.be/u4ZRQfkwU8Q?si=6oqNIEVAAoZ5aw7"
+  },
+  {
+    "nome": "LL Guitarra Pagodão v2",
+    "valor": 70,
+    "video": "https://youtu.be/oMSuwiTPuKw?si=VCkhC7WR0Oa6AsjF"
+  },
+  {
+    "nome": "LL Pagodao Rythm v2",
+    "valor": 70,
+    "video": "https://youtu.be/h0BoTh6bUBQ?si=PsUqKrSxQ8yqDZ_V"
+  },
+  {
+    "nome": "LL Guitarra Axe v2",
+    "valor": 80,
+    "video": "https://youtu.be/NU6ewsQQaaY?si=glnHb9jepQEISCvm"
+  },
+  {
+    "nome": "LL Bass Axé",
+    "valor": 70,
+    "video": "https://youtu.be/bO-R5UNPgmY?si=4d5CmBYOEIrPnbBV"
+  },
+  {
+    "nome": "LL Axé Rythm v2",
+    "valor": 80,
+    "video": "https://youtu.be/AUXgiprtqhA?si=XhG7ITJLwblC_7nF"
+  },
+  {
+    "nome": "LL Cavaco Prime v2",
+    "valor": 70,
+    "video": "https://youtu.be/jN1uVwx0B88?si=LGZxUbdDudA6rrVr"
+  },
+  {
+    "nome": "LL Acordeon Vaneira",
+    "valor": 60,
+    "video": "https://youtu.be/NQ6KcXt1OXc?si=gJi4RsHg7QH576bm"
+  },
+  {
+    "nome": "LL Violao Pagode Samba",
+    "valor": 60,
+    "video": "https://youtu.be/dl40SSHThfg?si=yIaLKSAEyd5aKKQB"
+  },
+  {
+    "nome": "LL Drums Manu Bahtidao",
+    "valor": 60,
+    "video": "https://youtu.be/FTg5oa1VHXU?si=ppkYm9OTD7LfU9iN"
+  },
+  {
+    "nome": "LL Vaneira Drums",
+    "valor": 60,
+    "video": "https://youtu.be/0DVtxwdI8t0?si=e86hWI8i5BfqHtUO"
+  },
+  {
+    "nome": "LL Violão Pop",
+    "valor": 60,
+    "video": "https://youtu.be/nuG8_3bNz8w?si=cOG3BcY11sIK0xEy"
+  },
+  {
+    "nome": "LL Acordeon Arrocha",
+    "valor": 60,
+    "video": "https://youtu.be/mO0MaAN0Uqg?si=LBGc3mUFK-6uR2Bw"
+  },
+  {
+    "nome": "LL Guitarra Calypso",
+    "valor": 60,
+    "video": "https://youtu.be/-dCxO11hSgc?si=lFhQdf2DKN2p91DH"
+  },
+  {
+    "nome": "LL Drums Felipe Amorim",
+    "valor": 60,
+    "video": "https://youtu.be/iLb__-DAIO4?si=L7NsgoxhYTnqSev5"
+  },
+  {
+    "nome": "LL Drums Zé Felipe",
+    "valor": 60,
+    "video": "https://youtu.be/d10c7aQLPgM?si=8O2SNR05q4-jhNvA"
+  },
+  {
+    "nome": "LL Drums Mari Fernandez",
+    "valor": 60,
+    "video": "https://youtu.be/BwIcLysROfM?si=1wk7wUq1fccgpRva"
+  },
+  {
+    "nome": "LL Bass Piseiro v2",
+    "valor": 60,
+    "video": "https://youtu.be/OcT97fnfB5M?si=L1JG-J7piPKxEdgq"
+  },
+  {
+    "nome": "LL Guitarra Piseiro v3",
+    "valor": 80,
+    "video": "https://youtu.be/uJG_RXyoeP4?si=UquVv9MGLk8m6rcZ"
+  },
+  {
+    "nome": "LL Pagode Drums e Percussion",
+    "valor": 60,
+    "video": "https://youtu.be/EHIwzcjL318?si=lI-5AlsOrgdU3BHn"
+  },
+  {
+    "nome": "LL Violão Vaneira",
+    "valor": 60,
+    "video": "https://youtu.be/y50aOUzONBU?si=jzjUAtO2S5pVPZdf"
+  },
+  {
+    "nome": "LL Iguinho e Lulinha Drums",
+    "valor": 60,
+    "video": "https://youtu.be/Sv2qJHQ0l-o?si=kjFKeJZelf_YbjBy"
+  },
+  {
+    "nome": "LL Bass Arrasta-Pe",
+    "valor": 40,
+    "video": "https://youtu.be/0nPWAixsqhg?si=fob55xx8SAigoZiT"
+  },
+  {
+    "nome": "LL Metais v3",
+    "valor": 80,
+    "video": "https://youtu.be/S0CqRq9a4TI?si=x_XKZCWPNa0P3__1"
+  },
+  {
+    "nome": "LL Forro Romantico Drums",
+    "valor": 60,
+    "video": "https://youtu.be/9bKunDA2A88?si=FOKFisqdS6SUo8te"
+  },
+  {
+    "nome": "LL Violão Arrocha 1.5 (Re-Edition)",
+    "valor": 60,
+    "video": "https://youtu.be/SB7_pE15rbw?si=jISOG-WijwKaN1JT"
+  },
+  {
+    "nome": "LL Guitarra Arrasta-Pe",
+    "valor": 40,
+    "video": "https://youtu.be/gCT1ernB7DI?si=2StyUzrHWL86OCtd"
+  },
+  {
+    "nome": "LL Arrasta-Pe Drums 1.5",
+    "valor": 60,
+    "video": "https://youtu.be/nke0JEdT8YA?si=pSY3Z1FoEqM6-z8N"
+  },
+  {
+    "nome": "LL Metais 2.0",
+    "valor": 80,
+    "video": "https://youtu.be/RArd9zPafhQ?si=RxUhCsSuN9dj6kwk"
+  },
+  {
+    "nome": "LL Arrocha Drums Nadson",
+    "valor": 60,
+    "video": "https://www.youtube.com/watch?v=RPNK-7NEPKw&list=RDRPNK-7NEPKw&start_radio=1"
+  },
+  {
+    "nome": "LL Guitarra Distorção",
+    "valor": 60,
+    "video": "https://youtu.be/ODTTJ7ZLjco?si=ZQcrh1wl9ZikOA77"
+  },
+  {
+    "nome": "LL Arrocha Percussao",
+    "valor": 60,
+    "video": "https://youtu.be/sOzFZulOUHQ?si=AFt8hQGvev1l01iS"
+  },
+  {
+    "nome": "LL Cavaco Prime",
+    "valor": 60,
+    "video": "https://youtu.be/eIi__vQi14Q?si=-v1QjTikaAzMnNva"
+  },
+  {
+    "nome": "LL Arrasta-Pe Drums",
+    "valor": 60,
+    "video": "https://www.youtube.com/watch?v=nAOiYoV1Y6U&list=RDnAOiYoV1Y6U&start_radio=1"
+  },
+  {
+    "nome": "LL Bass Arrocha",
+    "valor": 60,
+    "video": "https://youtu.be/7RV6pSg8waQ?si=bwoG2HBWWmoz64wL"
+  },
+  {
+    "nome": "LL Guitarra Arrocha",
+    "valor": 60,
+    "video": "https://youtu.be/TUUs3lmy3Y0?si=XZPMvAvAMJP9_wvs"
+  },
+  {
+    "nome": "LL Violao Arrocha",
+    "valor": 60,
+    "video": ""
+  },
+  {
+    "nome": "LL Arrocha Drums",
+    "valor": 70,
+    "video": "https://youtu.be/1C1FO1tHJyI?si=Sy9HazkMIzN6B5-2"
+  },
+  {
+    "nome": "LL Acordeon Piseiro 2.0",
+    "valor": 70,
+    "video": "https://youtu.be/v5kv8za6-90?si=F6QpoqKaEO-KHMO2"
+  },
+  {
+    "nome": "LL Piseiro Drums Summer",
+    "valor": 70,
+    "video": "https://youtu.be/iasVX_hPkS4?si=zFEf5FSzaeUu3yoV"
+  },
+  {
+    "nome": "LL Guitarra Piseiro 2.0",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=ElgxzAuavdY&list=RDElgxzAuavdY&start_radio=1"
+  },
+  {
+    "nome": "LL Guitarra Pagodão",
+    "valor": 70,
+    "video": "https://www.youtube.com/watch?v=vv-1QI_RbKM&pp=ygUTTEwgR3VpdGFycmEgUGFnb2Rhb9IHCQkICgGHKiGM7w%3D%3D"
+  },
+  {
+    "nome": "LL Pagodao Drums e Percussion",
+    "valor": 70,
+    "video": "https://youtu.be/EHIwzcjL318?si=lI-5AlsOrgdU3BHn"
+  },
+  {
+    "nome": "LL Axé Drums",
+    "valor": 60,
+    "video": "https://www.youtube.com/watch?v=Yzg5m4ZJw1g&t=20s"
+  },
+  {
+    "nome": "LL Guitarra Axé",
+    "valor": 60,
+    "video": "https://www.youtube.com/watch?v=NkO61zkS-C8&list=RDNkO61zkS-C8&start_radio=1"
+  },
+  {
+    "nome": "LL Axé Percussão",
+    "valor": 60,
+    "video": "https://www.youtube.com/watch?v=9rbJgipJmV0&list=RD9rbJgipJmV0&start_radio=1"
+  },
+  {
+    "nome": "LL Piseiro Drums 2.0",
+    "valor": 80,
+    "video": "https://www.youtube.com/watch?v=I0gpLLrcGe4&list=RDI0gpLLrcGe4&start_radio=1"
+  },
+  {
+    "nome": "LL Piseiro Drums 1.5",
+    "valor": 80,
+    "video": "https://youtu.be/6z0aRqURc4w?si=ZAI68b_qdnK2m7LA"
+  },
+  {
+    "nome": "LL Pe de Serra Drums e Perc",
+    "valor": 60,
+    "video": "https://youtu.be/LAh_N1dgdRI?si=kq3wqwhPs1NU1GKO"
+  },
+  {
+    "nome": "LL Acordeon Pé-de-Serra",
+    "valor": 60,
+    "video": "https://youtu.be/3h8xR4OmtH8?si=HDDBViN52uia_s6e"
+  },
+  {
+    "nome": "LL Guitarra Bregadeira",
+    "valor": 60,
+    "video": "https://youtu.be/93LWdjfAIfg?si=oIe6KFojIkQZO8XR"
+  },
+  {
+    "nome": "LL Bregadeira Drums",
+    "valor": 60,
+    "video": "https://youtu.be/IJNOLjdF_8o?si=BN-dy24wlx2L5giT"
+  },
+  {
+    "nome": "LL Percussao Piseiro",
+    "valor": 60,
+    "video": "https://youtu.be/I2Tu68topUU?si=IfW2expvkG41K0Po"
+  },
+  {
+    "nome": "LL Piseiro Bass",
+    "valor": 60,
+    "video": "https://youtu.be/5K1IOqGaP6g?si=PYO9vBxuCGhiZkbR"
+  },
+  {
+    "nome": "LL Guitarra Piseiro",
+    "valor": 60,
+    "video": "https://youtu.be/rUvw79orXZg?si=q4JE8l1OrC4MuXQy"
+  },
+  {
+    "nome": "LL Acordeon Piseiro",
+    "valor": 60,
+    "video": "https://youtu.be/wzNRhtxSIx0?si=yjudaYtzgVWlj7r6"
+  },
+  {
+    "nome": "LL WS Drums",
+    "valor": 60,
+    "video": "https://youtu.be/8onKDOVKCxE?si=HcoB1ZkR_9_UjRS5"
+  },
+  {
+    "nome": "LL Piseiro Drums v1",
+    "valor": 60,
+    "video": "https://youtu.be/OnQKHjpjvaQ?si=hbo9pwX4DWucZPSb"
+  },
+  {
+    "nome": "LL Metais",
+    "valor": 60,
+    "video": "https://youtu.be/KuUTx8NzAr4?si=lbaaAjGnjUO9IpJc"
+  },
+  {
+    "nome": "LL Sax v1.5",
+    "valor": 60,
+    "video": "https://youtu.be/YotFGV5IQl8?si=4ahVjkVZTQeqbfmK"
+  }
+];
+
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = LL_PRODUCTS;
+}
+
+if (typeof window !== "undefined") {
+  window.LL_PRODUCTS = LL_PRODUCTS;
+}
