1024e56342dbd391925483d79e1f2d0ee55879bb
llsamplese <llsamplesegravacoes@gmail.com>
2026-07-10 16:38:15 -0300

    Update product download links

--- data/download-links.js
+++ data/download-links.js
@@ -1,5 +1,4 @@
 module.exports = {
-  "LL Guitarra Arrocha v3": "https://we.tl/t-jcThJ6pOcVB4k7Pk",
   "LL Bass Arrocha v3": "https://we.tl/t-VmsOdC1F66V6zPoJ",
   "LL Pacote Produção Raphaela Santos": "https://we.tl/t-gjGBHcpx12CzaODG",
   "LL Drums Raphaela Santos": "https://we.tl/t-5HvABwb3JoHbDjGC",
