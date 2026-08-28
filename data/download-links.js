fafb572fba8a4253c7f86ae9c2757009c7ce69bd
llsamplese <llsamplesegravacoes@gmail.com>
2026-08-28 19:02:27 -0300

    Update product download links

--- data/download-links.js
+++ data/download-links.js
@@ -1,4 +1,5 @@
 module.exports = {
+  "LL Drums Cumbia": "https://we.tl/t-98aoR72u92PvxoeZ",
   "LL Drums Heitor Santos": "https://we.tl/t-ghGtTtk3hg2wLYVL",
   "LL Piano Romântico v2": "https://we.tl/t-y7uQAiHB0vSAfMbs",
   "LL Guitarra Arrocha v3": "https://we.tl/t-jcThJ6pOcVB4k7Pk",
